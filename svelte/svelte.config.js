c87e90a5da551e163d93eb9bd1e59e007ab5adbd
Aidan Goldthorpe <aidan.goldthorpe@gmail.com>
2024-02-24 10:21:57 +1100

    Upgrade Sveltekit to version 2

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -9,13 +9,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess({
-		style: {
-			css: {
-				postcss: join(__dirname, "postcss.config.cjs")
+	preprocess: [
+		vitePreprocess({
+			style: {
+				css: {
+					postcss: join(__dirname, 'postcss.config.cjs')
+				}
 			}
-		}
-	})],
+		})
+	],
 
 	kit: {
 		adapter: adapter({
